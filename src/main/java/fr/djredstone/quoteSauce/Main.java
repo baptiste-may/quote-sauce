ac764520261052ea2c5937f351c408df9f5408a5
DjRedstone <djredstone64@gmail.com>
2022-06-06 11:31:18 +0200

    Ajout d'un message dans la console lors de l'activation du bot

--- src/main/java/fr/djredstone/quoteSauce/Main.java
+++ src/main/java/fr/djredstone/quoteSauce/Main.java
@@ -5,23 +5,37 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-public class Main {
+import org.jetbrains.annotations.NotNull;
+
+public class Main implements EventListener {
 
     public static final String prefix = ",";
     public static JDA jda;
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws LoginException, InterruptedException {
 
         JDABuilder builder = JDABuilder.createDefault(System.getenv().get("TOKEN"));
 
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
-        builder.setActivity(Activity.watching("être développé"));
+        builder.setActivity(Activity.playing("être développé"));
 
+        builder.addEventListeners(new Main());
         jda = builder.build();
 
+        jda.awaitReady();
+
         new Setup(jda);
+    }
+
+    @Override
+    public void onEvent(@NotNull GenericEvent event) {
+        if (event instanceof ReadyEvent)
+            System.out.println("Quote Sauce Bot is online!");
     }
 
 }
